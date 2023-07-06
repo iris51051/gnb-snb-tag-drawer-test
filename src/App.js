35f3d65574213ea08204571b66d01ea3a9d8643d
iris5105 <iris5105@bispring.co.kr>
2023-07-06 18:26:31 +0900

    raouter 설치

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   VideoCameraOutlined,
   MenuUnfoldOutlined,
 } from "@ant-design/icons";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Button, Layout, Menu, theme } from "antd";
 import { useState } from "react";
 
@@ -20,106 +21,108 @@ const App = () => {
   } = theme.useToken();
 
   return (
-    <Layout>
-      <Header
-        style={{
-          padding: 0,
-          background: colorBgContainer,
-        }}
-      >
-        <Button
-          type="text"
-          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-          onClick={() => setCollapsed(!collapsed)}
-          style={{
-            fontSize: "16px",
-            width: 64,
-            height: 64,
-          }}
-        />
-      </Header>
-
+    <Router>
       <Layout>
-        <Sider
-          trigger={null}
-          collapsible
-          collapsed={collapsed}
-          style={{ height: "100vh" }}
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
         >
-          <div className="demo-logo-vertical" />
-          <Menu
-            theme="dark"
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            items={[
-              {
-                key: "0",
-                icon: <UserOutlined />,
-                label: "통합대시보드",
-              },
-              {
-                key: "1",
-                icon: <UserOutlined />,
-                label: "통합대시보드",
-              },
-              {
-                key: "2",
-                icon: <VideoCameraOutlined />,
-                label: "대시보드",
-              },
-              {
-                key: "3",
-                icon: <UploadOutlined />,
-                label: "보고서 다운로드",
-              },
-              {
-                key: "4",
-                icon: <UploadOutlined />,
-                label: "모니터링 알람",
-                children: [
-                  {
-                    key: "4-1",
-                    label: "알람 설정",
-                  },
-                  {
-                    key: "4-2",
-                    label: "알람 실행 스토리",
-                  },
-                ],
-              },
-              {
-                key: "5",
-                icon: <UploadOutlined />,
-                label: "매체 파일 업로드",
-              },
-              {
-                key: "6",
-                icon: <UploadOutlined />,
-                label: "매체 데이터 내보내기",
-              },
-              {
-                key: "7",
-                icon: <UploadOutlined />,
-                label: "매체 데이터 다운로드",
-              },
-            ]}
-          />
-        </Sider>
-        <Layout>
-          <Content
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
             style={{
-              padding: 24,
-              minHeight: 280,
-              background: colorBgContainer,
-              justifyContent: "space-between",
-              alignItems: "center",
+              fontSize: "16px",
+              width: 64,
+              height: 64,
             }}
+          />
+        </Header>
+
+        <Layout>
+          <Sider
+            trigger={null}
+            collapsible
+            collapsed={collapsed}
+            style={{ height: "100vh" }}
           >
-            <div></div>
-          </Content>
+            <div className="demo-logo-vertical" />
+            <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={["1"]}
+              items={[
+                {
+                  key: "0",
+                  icon: <UserOutlined />,
+                  label: <Link to="/">통합대시보드</Link>,
+                },
+                {
+                  key: "1",
+                  icon: <UserOutlined />,
+                  label: <Link to="/">통합대시보드</Link>,
+                },
+                {
+                  key: "2",
+                  icon: <VideoCameraOutlined />,
+                  label: <Link to="/">대시보드</Link>,
+                },
+                {
+                  key: "3",
+                  icon: <UploadOutlined />,
+                  label: <Link to="/">보고서 다운로드</Link>,
+                },
+                {
+                  key: "4",
+                  icon: <UploadOutlined />,
+                  label: <Link to="/">모니터링 알람</Link>,
+                  children: [
+                    {
+                      key: "4-1",
+                      label: <Link to="/">알람 설정</Link>,
+                    },
+                    {
+                      key: "4-2",
+                      label: <Link to="/">알람 실행 스토리</Link>,
+                    },
+                  ],
+                },
+                {
+                  key: "5",
+                  icon: <UploadOutlined />,
+                  label: <Link to="/">매체 파일 업로드</Link>,
+                },
+                {
+                  key: "6",
+                  icon: <UploadOutlined />,
+                  label: <Link to="/">매체 데이터 내보내기</Link>,
+                },
+                {
+                  key: "7",
+                  icon: <UploadOutlined />,
+                  label: <Link to="/">매체 데이터 다운로드</Link>,
+                },
+              ]}
+            />
+          </Sider>
+          <Layout>
+            <Content
+              style={{
+                padding: 24,
+                minHeight: 280,
+                background: colorBgContainer,
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <div></div>
+            </Content>
+          </Layout>
         </Layout>
       </Layout>
-    </Layout>
+    </Router>
   );
 };
 
